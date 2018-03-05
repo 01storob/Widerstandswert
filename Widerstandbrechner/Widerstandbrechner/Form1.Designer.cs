d174fb9f052849ae3530ba7ccbb6dba64bde4991
01storob@rgtfo-me.it <01storob@rgtfo-me.it>
2018-03-05 18:25:02 +0100

    Probe gemacht

---
+++ Widerstandbrechner/Widerstandbrechner/Form1.Designer.cs
@@ -0,0 +1,265 @@
+﻿namespace Widerstandbrechner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.btn_Brechnen = new System.Windows.Forms.Button();
+            this.Toleranz = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Ergebnis = new System.Windows.Forms.TextBox();
+            this.Ergebnis2 = new System.Windows.Forms.TextBox();
+            this.btn_clear = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Schwarz",
+            "Braun",
+            "Rot",
+            "Orange",
+            "Gelb",
+            "Grün",
+            "Blau",
+            "Violett",
+            "Grau",
+            "Weiß"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 79);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(120, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Schwarz",
+            "Braun",
+            "Rot",
+            "Orange",
+            "Gelb",
+            "Grün",
+            "Blau",
+            "Violett",
+            "Grau",
+            "Weiß"});
+            this.comboBox2.Location = new System.Drawing.Point(157, 79);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(120, 28);
+            this.comboBox2.TabIndex = 1;
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "Schwarz",
+            "Braun",
+            "Rot",
+            "Orange",
+            "Gelb",
+            "Grün",
+            "Blau",
+            "Violett"});
+            this.comboBox3.Location = new System.Drawing.Point(318, 79);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(120, 28);
+            this.comboBox3.TabIndex = 2;
+            // 
+            // btn_Brechnen
+            // 
+            this.btn_Brechnen.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Brechnen.Location = new System.Drawing.Point(382, 171);
+            this.btn_Brechnen.Name = "btn_Brechnen";
+            this.btn_Brechnen.Size = new System.Drawing.Size(117, 65);
+            this.btn_Brechnen.TabIndex = 3;
+            this.btn_Brechnen.Text = "Berechnen";
+            this.btn_Brechnen.UseVisualStyleBackColor = true;
+            this.btn_Brechnen.Click += new System.EventHandler(this.btn_Berechnen_Click);
+            // 
+            // Toleranz
+            // 
+            this.Toleranz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Toleranz.FormattingEnabled = true;
+            this.Toleranz.Items.AddRange(new object[] {
+            "Silber",
+            "Gold",
+            "Braun",
+            "Rot",
+            "Grün",
+            "Blau",
+            "Violett"});
+            this.Toleranz.Location = new System.Drawing.Point(470, 79);
+            this.Toleranz.Name = "Toleranz";
+            this.Toleranz.Size = new System.Drawing.Size(120, 28);
+            this.Toleranz.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(16, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 24);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Wert1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(162, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Wert2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(322, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 24);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Wert3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(475, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(85, 24);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Tolerenz";
+            // 
+            // Ergebnis
+            // 
+            this.Ergebnis.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ergebnis.Location = new System.Drawing.Point(82, 171);
+            this.Ergebnis.Name = "Ergebnis";
+            this.Ergebnis.Size = new System.Drawing.Size(160, 32);
+            this.Ergebnis.TabIndex = 9;
+            // 
+            // Ergebnis2
+            // 
+            this.Ergebnis2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Ergebnis2.Location = new System.Drawing.Point(274, 171);
+            this.Ergebnis2.Name = "Ergebnis2";
+            this.Ergebnis2.Size = new System.Drawing.Size(100, 32);
+            this.Ergebnis2.TabIndex = 10;
+            // 
+            // btn_clear
+            // 
+            this.btn_clear.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_clear.Location = new System.Drawing.Point(505, 171);
+            this.btn_clear.Name = "btn_clear";
+            this.btn_clear.Size = new System.Drawing.Size(85, 65);
+            this.btn_clear.TabIndex = 11;
+            this.btn_clear.Text = "C";
+            this.btn_clear.UseVisualStyleBackColor = true;
+            this.btn_clear.Click += new System.EventHandler(this.btn_clear_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(86, 136);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(149, 24);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Widerstandswert";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(278, 136);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(84, 24);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Toleranz";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(627, 327);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btn_clear);
+            this.Controls.Add(this.Ergebnis2);
+            this.Controls.Add(this.Ergebnis);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Toleranz);
+            this.Controls.Add(this.btn_Brechnen);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Button btn_Brechnen;
+        private System.Windows.Forms.ComboBox Toleranz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox Ergebnis;
+        private System.Windows.Forms.TextBox Ergebnis2;
+        private System.Windows.Forms.Button btn_clear;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Widerstandbrechner/Widerstandbrechner/Form1.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Widerstandbrechner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Berechnen_Click(object sender, EventArgs e)
+        {
+
+            switch (comboBox1.Text)
+            {
+                case "Schwarz":
+                    Ergebnis.Text = "0";
+                    break;
+                case "Braun":
+                    Ergebnis.Text = "1";
+                    break;
+                case "Rot":
+                    Ergebnis.Text = "2";
+                    break;
+                case "Orange":
+                    Ergebnis.Text = "3";
+                    break;
+                case "Gelb":
+                    Ergebnis.Text = "4";
+                    break;
+                case "Grün":
+                    Ergebnis.Text = "5";
+                    break;
+                case "Blau":
+                    Ergebnis.Text = "6";
+                    break;
+                case "Violett":
+                    Ergebnis.Text = "7";
+                    break;
+                case "Grau":
+                    Ergebnis.Text = "8";
+                    break;
+                case "Weiß":
+                    Ergebnis.Text = "9";
+                    break;
+            }
+            switch (comboBox2.Text)
+            {
+                case "Schwarz":
+                    Ergebnis.Text += "0";
+                    break;
+                case "Braun":
+                    Ergebnis.Text += "1";
+                    break;
+                case "Rot":
+                    Ergebnis.Text += "2";
+                    break;
+                case "Orange":
+                    Ergebnis.Text += "3";
+                    break;
+                case "Gelb":
+                    Ergebnis.Text += "4";
+                    break;
+                case "Grün":
+                    Ergebnis.Text += "5";
+                    break;
+                case "Blau":
+                    Ergebnis.Text += "6";
+                    break;
+                case "Violett":
+                    Ergebnis.Text += "7";
+                    break;
+                case "Grau":
+                    Ergebnis.Text += "8";
+                    break;
+                case "Weiß":
+                    Ergebnis.Text += "9";
+                    break;
+            }
+
+            switch (comboBox3.Text)
+            {
+                case "Schwarz":
+                    Ergebnis.Text += "";
+                    break;
+                case "Braun":
+                    Ergebnis.Text += "0";
+                    break;
+                case "Rot":
+                    Ergebnis.Text += "00";
+                    break;
+                case "Orange":
+                    Ergebnis.Text += "k";
+                    break;
+                case "Gelb":
+                    Ergebnis.Text += "0k";
+                    break;
+                case "Grün":
+                    Ergebnis.Text += "00k";
+                    break;
+                case "Blau":
+                    Ergebnis.Text += "M";
+                    break;
+                case "Violett":
+                    Ergebnis.Text += "0M";
+                    break;
+                //case "Grau":
+                //    Ergebnis.Text += "00M";
+                //    break;
+                //case "Weiß":
+                //    Ergebnis.Text += "G";
+                //    break;
+            }
+
+            switch (Toleranz.Text)
+            {
+                case "Silber":
+                    Ergebnis2.Text += " 10%";
+                    break;
+                case "Gold":
+                    Ergebnis2.Text += " 5%";
+                    break;
+                case "Braun":
+                    Ergebnis2.Text += " 1%";
+                    break;
+                case "Rot":
+                    Ergebnis2.Text += " 2%";
+                    break;
+                case "Grün":
+                    Ergebnis2.Text += " 0.5%";
+                    break;
+                case "Blau":
+                    Ergebnis2.Text += " 0.25%";
+                    break;
+                case "Violett":
+                    Ergebnis2.Text += " 0.1%";
+                    break;
+
+                    //Git probe
+            }
+        }
+
+        private void btn_clear_Click(object sender, EventArgs e)
+        {
+            Ergebnis.Clear();
+            Ergebnis2.Clear();
+            Toleranz.Text = "";
+            comboBox1.Text = "";
+            comboBox2.Text = "";
+            comboBox3.Text = "";
+        }
+
+    }
+}
+    
